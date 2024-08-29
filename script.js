6516a118e7d03b00b4f8f6a4d41840f9566668ad
Anikesh Chouhan <anikeshmaheshwari558@gmail.com>
2024-08-29 11:27:15 +0530

    start

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+console.log(months);
+
+console.log(form);
+console.log(containerWorkouts);
+console.log(inputType);
+console.log(inputDistance);
+console.log(inputDuration);
+console.log(inputCadence);
+console.log(inputElevation);
